c859b55fd00f99c16cd2b821afb6a530f5835503
dlichy <51176531+dlichy@users.noreply.github.com>
2021-04-01 12:57:18 -0400

    Update prepare_diligent_dataset.sh

--- scripts/prepare_diligent_dataset.sh
+++ scripts/prepare_diligent_dataset.sh
@@ -5,9 +5,9 @@ cd data
 url="https://www.dropbox.com/s/hdnbh526tyvv68i/DiLiGenT.zip?dl=0"
 name="DiLiGenT"
 
-#wget $url -O ${name}.zip
-#unzip ${name}.zip
-#rm ${name}.zip
+wget $url -O ${name}.zip
+unzip ${name}.zip
+rm ${name}.zip
 
 cd ${name}/pmsData/
 cp ballPNG/filenames.txt names.txt
